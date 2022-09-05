933f162cffcd6b94edb4873d86d4067846ce0d6b
Miguel González <migueljgonz@gmail.com>
2022-09-05 20:30:02 +0200

    Delete all migrations to start over
    Signed-off-by: Miguel González <migueljgonz@gmail.com>

--- src/StorytellerBot/Data/AdventureContext.cs
+++ src/StorytellerBot/Data/AdventureContext.cs
@@ -17,27 +17,10 @@ public class AdventureContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity
-                .HasOne(u => u.CommandProgress)
-                .WithOne(cp => cp.User)
-                .HasForeignKey<User>(u => u.CommandProgressId)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            entity
-                .HasOne(u => u.CurrentGame)
-                .WithOne(cg => cg.User)
-                .HasForeignKey<User>(u => u.CurrentGameId)
-                .OnDelete(DeleteBehavior.SetNull);
-        });
+        modelBuilder.Entity<User>();
         modelBuilder.Entity<CommandProgress>();
         modelBuilder.Entity<Adventure>();
         modelBuilder.Entity<SavedStatus>();
-        modelBuilder.Entity<CurrentGame>()
-            .HasOne(cg => cg.SavedStatus)
-            .WithOne()
-            .HasForeignKey<CurrentGame>(cg => cg.SavedStatusId)
-            .OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<CurrentGame>();
     }
 }

938f204073089e7b20ea288dd23b958ffaf0e4b8
Miguel González <migueljgonz@gmail.com>
2022-09-08 21:58:56 +0200

    Remove Startup, configure log4net
    Signed-off-by: Miguel González <migueljgonz@gmail.com>

--- src/StorytellerBot/Program.cs
+++ src/StorytellerBot/Program.cs
@@ -1,10 +1,78 @@
-using StorytellerBot;
+using Microsoft.EntityFrameworkCore;
+using StorytellerBot.Data;
+using StorytellerBot.Services;
+using StorytellerBot.Services.Conversations;
+using StorytellerBot.Settings;
+using Telegram.Bot;
 
-CreateHostBuilder(args).Build().Run();
+var builder = WebApplication.CreateBuilder(args);
 
-static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Startup>();
-        });
+#region Logging
+
+builder.Logging.AddLog4Net();
+
+#endregion
+
+#region Services
+
+builder.Services.AddDbContext<AdventureContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("AdventureContextSQLite")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddHostedService<ConfigureWebhook>();
+builder.Services.AddHostedService<ConfigureCommands>();
+
+var botConfigurationSection = builder.Configuration.GetSection(nameof(BotConfiguration));
+
+builder.Services.Configure<BotConfiguration>(botConfigurationSection);
+builder.Services.Configure<MessageSettings>(builder.Configuration.GetSection("Message"));
+
+var botToken = botConfigurationSection
+    .GetValue<string>(nameof(BotConfiguration.BotToken));
+builder.Services
+    .AddHttpClient("tgwebhook")
+    .AddTypedClient<ITelegramBotClient>(
+        httpClient => new TelegramBotClient(botToken, httpClient));
+
+builder.Services.AddScoped<AdventureRepository>();
+builder.Services.AddScoped<IConversationFactory, ConversationFactory>();
+builder.Services.AddScoped<IResponseSender, ResponseSender>();
+builder.Services.AddScoped<IAdventureWriter, AdventureWriter>();
+builder.Services.AddScoped<IMediaLocator, MediaLocator>();
+
+builder.Services.AddTransient<CallbackConversation>();
+builder.Services.AddTransient<TextConversation>();
+builder.Services.AddTransient<StartCommandConversation>();
+builder.Services.AddTransient<RestartCommandConversation>();
+builder.Services.AddTransient<ListCommandConversation>();
+builder.Services.AddTransient<NoopConversation>();
+
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson(setup => setup.UseCamelCasing(true));
+
+#endregion
+
+#region Application
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseRouting();
+app.UseCors();
+
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+app.Run();
+
+#endregion
